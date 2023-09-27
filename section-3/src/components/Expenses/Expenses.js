7869d890b0558ba6073c2ffdcaea13b4bcc177e2
raymondhua <rayrayhua@outlook.com>
2023-09-27 16:19:29 +1300

    Completed assignment 2

--- section-3/src/components/Expenses/Expenses.js
+++ section-3/src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
+
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear => {
+    console.log("Expenses.js");
+    
+    setFilteredYear(selectedYear);
+    console.log(filteredYear);
+  }
   return (
+    <div>
     <Card className="expenses">
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,6 +38,7 @@ function Expenses(props) {
         date={props.items[3].date}
       ></ExpenseItem>
     </Card>
+    </div>
   );
 }
 
