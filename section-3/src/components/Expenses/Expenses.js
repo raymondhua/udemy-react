cbf347a6aedc45fa8dcf0caffd2b65a9ee0a59fa
raymondhua <rayrayhua@outlook.com>
2023-10-04 14:57:51 +1300

    Completed 90

--- section-3/src/components/Expenses/Expenses.js
+++ section-3/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -16,18 +17,7 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear
   });
 
-  let expensesContent = <p>No expenses found.</p>;
 
-  if (filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem 
-        key={expense.id}
-        title={expense.title} 
-        amount={expense.amount} 
-        date={expense.date}
-      />
-    ))
-  }
 
   return (
     <div>
@@ -36,7 +26,7 @@ function Expenses(props) {
         elected={filteredYear} 
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
   );
