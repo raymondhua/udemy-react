1d90637a5bb7ac62c4a43a875dec48df472017d2
raymondhua <rayrayhua@outlook.com>
2023-11-08 16:45:44 +1300

    Add ref to ResultModal dialog and update TimerChallange to show modal on timer expiration

--- section-8/src/components/TimerChallange.jsx
+++ section-8/src/components/TimerChallange.jsx
@@ -3,12 +3,14 @@ import ResultModal from "./ResultModal";
 
 export default function TimerChallange({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -19,10 +21,9 @@ export default function TimerChallange({ title, targetTime }) {
 
   return (
     <>
-    {timerExpired && <ResultModal result="lost" targetTime={targetTime} />}
+    <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
     <section className="challenge">
       <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
