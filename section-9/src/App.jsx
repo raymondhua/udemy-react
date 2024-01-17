6e0887bd59a59d4da439ed11148531d7e034e470
raymondhua <rayrayhua@outlook.com>
2024-01-17 14:34:29 +1300

    Added section 9 starting project

---
+++ section-9/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSideBar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <main className="h-screen">
+      <ProjectsSideBar />
+    </main>
+  );
+}
+
+export default App;
